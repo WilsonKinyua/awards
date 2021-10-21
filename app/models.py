70632dc75a6d90ef9c9f3ec7f8633a70dd40e815
wilsonkinyua <wilsonkinyuam@gmail.com>
2021-10-21 23:38:13 +0300

    initial commit

---
+++ app/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+import datetime as dt
+
+# cloudinary
+from cloudinary.models import CloudinaryField
+from django.contrib.auth.models import User
+
+# Create your models here.
+
+
