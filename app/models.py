0ae0d2e37c6293d772beb982fb4fc163da76ac46
wilsonkinyua <wilsonkinyuam@gmail.com>
2021-10-21 23:38:39 +0300

    project model defined

--- app/models.py
+++ app/models.py
@@ -8,3 +8,15 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 
+# project models
+class Project(models.Model):
+    title = models.CharField(max_length=250)
+    description = models.CharField(max_length=250)
+    image = CloudinaryField("image")
+    url = models.URLField(blank=True)
+
+    def __str__(self):
+        return self.title
+
+
+
