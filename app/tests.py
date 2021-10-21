3d6b4066387c09386c32f05b5a4de872fd72fa7d
wilsonkinyua <wilsonkinyuam@gmail.com>
2021-10-22 01:36:11 +0300

    ProfileTestClass passed

--- app/tests.py
+++ app/tests.py
@@ -5,7 +5,7 @@ from .models import Project, Profile, DesignRate, UsabilityRate, ContentRate
 # Create your tests here.
 
 
-class ProjectTestClass(TestCase):
+class ProjectTestClass(TestCase):  # Project class test
     def setUp(self):
         # create a user
         user = User.objects.create(
@@ -32,3 +32,31 @@ class ProjectTestClass(TestCase):
         self.project.delete_project()
         projects = Project.objects.all()
         self.assertTrue(len(projects) == 0)
+
+
+class ProfileTestClass(TestCase):  # Profile class test
+    def setUp(self):
+        # create a user
+        user = User.objects.create(
+            username="test_user", first_name="mzee", last_name="mzima"
+        )
+
+        self.profile = Profile(
+            bio="Test Bio",
+            user=user,
+            contact="Test Contact",
+        )
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.profile, Profile))
+
+    def test_save_method(self):
+        self.profile.save_profile()
+        profiles = Profile.objects.all()
+        self.assertTrue(len(profiles) > 0)
+
+    def test_delete_method(self):
+        self.profile.save_profile()
+        self.profile.delete_profile()
+        profiles = Profile.objects.all()
+        self.assertTrue(len(profiles) == 0)
