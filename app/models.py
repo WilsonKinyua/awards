2e705cdd382c32edf70b2e4ec3de6c3ed5d9b05a
wilsonkinyua <wilsonkinyuam@gmail.com>
2021-10-21 23:38:56 +0300

    profile model defined

--- app/models.py
+++ app/models.py
@@ -19,4 +19,23 @@ class Project(models.Model):
         return self.title
 
 
+# profile models
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    profile_photo = CloudinaryField("image")
+    bio = models.TextField(max_length=250)
+    contact = models.CharField(max_length=250)
 
+    def save_profile(self):
+        self.save()
+
+    def delete_profile(self):
+        self.delete()
+
+    @classmethod
+    def filter_by_id(cls, id):
+        profile = Profile.objects.filter(user=id).first()
+        return profile
+
+    def __str__(self):
+        return self.user.username
