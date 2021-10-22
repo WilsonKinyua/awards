98b3f4ae55c0a95915ff42078e6c1d7751ae1580
wilsonkinyua <wilsonkinyuam@gmail.com>
2021-10-23 01:25:34 +0300

    login profile bug fixed

--- app/urls.py
+++ app/urls.py
@@ -7,6 +7,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path("", views.index, name="index"),
     path("profile/", views.profile, name="profile"),
+    path("accounts/profile/", views.profile, name="profile"),
     path("profile/update/", views.update_profile, name="update_profile"),
     path("project/save/", views.save_project, name="save_project"),
     path("project/<int:project_id>/", views.project_details, name="project_details"),
