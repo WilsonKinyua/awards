c470020dfea7ba11565f6b5840dd55b4d1a700c9
wilsonkinyua <wilsonkinyuam@gmail.com>
2021-10-23 14:52:33 +0300

    profile page setup

--- app/urls.py
+++ app/urls.py
@@ -4,13 +4,10 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', views.index, name='index'),
-
+    path("", views.index, name="index"),
+    path("profile/", views.profile, name="profile"),
 ]
 
 
-
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL,
-                          document_root=settings.MEDIA_ROOT)
-                          
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- app/views.py
+++ app/views.py
@@ -1,7 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from .models import *
+import cloudinary
+import cloudinary.uploader
+import cloudinary.api
 
 # Create your views here.
 
 
 def index(request):
     return render(request, 'index.html')
+
+
+@login_required(login_url='/accounts/login/')
+def profile(request): # view profile
+    current_user = request.user
+    return render(request, 'profile.html')
